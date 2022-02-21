934d846dc99e40a7e9e0d7c94c71e432cd74f6a6
patrickmcgranaghan <petrarch1603@gmail.com>
2022-02-20 19:02:17 -0700

    minor formatting

--- peel_orange.py
+++ peel_orange.py
@@ -26,7 +26,7 @@ from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtWidgets import QAction
 
 # Initialize Qt resources from file resources.py
-from .resources import *
+# from .resources import *
 # Import the code for the dialog
 from .peel_orange_dialog import PeelOrangeDialog
 import os.path
@@ -81,7 +81,6 @@ class PeelOrange:
         """
         # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
         return QCoreApplication.translate('PeelOrange', message)
-
 
     def add_action(
         self,
