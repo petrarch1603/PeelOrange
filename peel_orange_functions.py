f4ad251c6bfda2fdc208a7f021aebd242214a715
patrickmcgranaghan <petrarch1603@gmail.com>
2022-02-26 15:32:02 -0700

    add "get cell size" function

--- peel_orange_functions.py
+++ peel_orange_functions.py
@@ -15,3 +15,9 @@ def read_metadata_txt(filename: str) -> dict:
     parser.read(get_file_path(filename))
     metadata.extend(parser.items('general'))
     return dict(metadata)
+
+
+def get_cell_size(lyr, divisible=100):
+    # Get shorter distance of sides of extent
+    my_min = min(lyr.extents().width(), lyr.extents().height())
+    return int(my_min/100)
