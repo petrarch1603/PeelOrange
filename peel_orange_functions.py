72d0def669631d07317fe87d1bc7d3c6ff6b7a4e
patrickmcgranaghan <petrarch1603@gmail.com>
2022-02-26 16:18:32 -0700

    add create centroids function

--- peel_orange_functions.py
+++ peel_orange_functions.py
@@ -1,4 +1,6 @@
 import os
+import processing
+from qgis.core import QgsProcessing, QgsVectorLayer
 import configparser
 
 
@@ -19,17 +21,29 @@ def read_metadata_txt(filename: str) -> dict:
 
 def get_cell_size(lyr, divisible=100):
     # Get shorter distance of sides of extent
-    my_min = min(lyr.extents().width(), lyr.extents().height())
+    my_min = min(lyr.extent().width(), lyr.extent().height())
+    print(my_min)
     return int(my_min/100)
+
 
 def create_grid(lyr):
     my_cell_size = get_cell_size(lyr)
+    print(my_cell_size)
     hex_dict = {'CRS': lyr.crs(),
                 'EXTENT': lyr.extent(),
                 'HOVERLAY': 0,
                 'HSPACING': my_cell_size,
-                'OUTPUT': 'memory',
+                'OUTPUT': QgsProcessing.TEMPORARY_OUTPUT,
                 'TYPE': 4,  # 4 is for Hexagons
                 'VOVERLAY': 0,
                 'VSPACING': my_cell_size}
-    return processing.run('native:creategrid', hex_dict)
+    return processing.run('native:creategrid', hex_dict)['OUTPUT']
+
+
+def create_centroids(lyr):
+    centroid_dict = {'ALL_PARTS': True,
+                     'INPUT': lyr,
+                     'OUTPUT': QgsProcessing.TEMPORARY_OUTPUT}
+    return processing.run('native:centroids', centroid_dict)['OUTPUT']
+
+
