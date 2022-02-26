8a6de8076ea44bafe5eb9bda083ac5addb3abf48
patrickmcgranaghan <petrarch1603@gmail.com>
2022-02-26 15:36:29 -0700

    add create grid function

--- peel_orange_functions.py
+++ peel_orange_functions.py
@@ -21,3 +21,15 @@ def get_cell_size(lyr, divisible=100):
     # Get shorter distance of sides of extent
     my_min = min(lyr.extents().width(), lyr.extents().height())
     return int(my_min/100)
+
+def create_grid(lyr):
+    my_cell_size = get_cell_size(lyr)
+    hex_dict = {'CRS': lyr.crs(),
+                'EXTENT': lyr.extent(),
+                'HOVERLAY': 0,
+                'HSPACING': my_cell_size,
+                'OUTPUT': 'memory',
+                'TYPE': 4,  # 4 is for Hexagons
+                'VOVERLAY': 0,
+                'VSPACING': my_cell_size}
+    return processing.run('native:creategrid', hex_dict)
