803109f92018d8dd19b4943138c88142931090a5
patrickmcgranaghan <petrarch1603@gmail.com>
2022-03-04 04:39:25 -0700

    minor formatting

--- peel_orange.py
+++ peel_orange.py
@@ -44,7 +44,6 @@ class PeelOrange:
 
     def __init__(self, iface):
         """Constructor.
-
         :param iface: An interface instance that will be passed to this class
             which provides the hook by which you can manipulate the QGIS
             application at run time.
