d234eabaa4962daa3cb0a5c05e2ca0d6be4162c4
patrickmcgranaghan <petrarch1603@gmail.com>
2022-03-06 11:23:15 -0700

    created StatAnalysis class

--- stat_analysis.py
+++ stat_analysis.py
@@ -2,10 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def get_stats(data_list):
-    my_stat_dict ={}
-    my_stat_dict['mean'] = np.mean(data_list)
-    my_stat_dict['std'] = np.std(data_list)
-    my_stat_dict['max'] = np.max(data_list)
-    my_stat_dict['min'] = np.min(data_list)
-    return my_stat_dict
+class StatAnalysis:
+    def __init__(self, lyr):
+        self.field_name = 'scale_dist'
+        self.lyr = lyr
+        self.data_list = self.get_data_list()
+        self.stats_dict = self.get_stats_dict()
+
+    def get_data_list(self):
+        my_data_points = []
+        field_index = self.lyr.fields().indexFromName(self.field_name)
+        for i in self.lyr.getFeatures():
+            my_data_points.append(i.attributes()[field_index])
+        return my_data_points
+
+    def get_stats_dict(self):
+        return {'mean': np.mean(self.data_list),
+                'std': np.std(self.data_list),
+                'max': np.max(self.data_list),
+                'min': np.min(self.data_list)}
+
